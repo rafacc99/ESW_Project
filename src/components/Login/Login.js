74777718e4f45ad464671d5f4e5bdb96bf3c7a6b
alexoliveira2000 <57905710+alexoliveira2000@users.noreply.github.com>
2020-03-10 17:50:15 +0000

    #Update: Teste

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Login() {
     return (
         <div>
-            <h1 className="font-italic">LOGIN</h1>
+          <h1 className="font-italic">LOGIN22</h1>
+          <h1 className="font-italic">Test</h1>
         </div>
     );
 }
